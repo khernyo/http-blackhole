06bd61e0010b31a5eaaf1d13f6fe696e695e4ce0
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2015-08-31 21:50:04 +0200

    Show request body

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 extern crate hyper;
 
-use std::io::Write;
+use std::io::{Read, Write};
 
 use hyper::Server;
 use hyper::server::Request;
@@ -9,13 +9,18 @@ use hyper::server::Response;
 use hyper::net::Fresh;
 
 fn log(request: Request, response: Response<Fresh>) {
-    let (socket_addr, method, headers, request_uri, http_version, _) = request.deconstruct();
+    let (socket_addr, method, headers, request_uri, http_version, mut body) = request.deconstruct();
     println!("----------------------------------------");
     println!("Request from {:?}", socket_addr);
     println!("Http version: {:?}", http_version);
     println!("Method: {:?}", method);
     println!("Headers: {:?}", headers);
     println!("Uri: {:?}", request_uri);
+
+    let mut body_s = String::new();
+    body.read_to_string(&mut body_s).unwrap();
+    println!("Body: {:?}", body_s);
+
     let mut res = response.start().unwrap();
     res.write_all(b"").unwrap();
     res.end().unwrap();
